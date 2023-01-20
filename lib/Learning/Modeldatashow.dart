648ef3ca99df9f65e97eb4a3853b1ac1d46eb776
Riddhi Desai <desairiddhi15@gmail.com>
2023-01-21 03:06:11 +0530

    changes in learning

---
+++ lib/Learning/Modeldatashow.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class ModelDataShow extends StatefulWidget {
+  const ModelDataShow({super.key});
+
+  @override
+  State<ModelDataShow> createState() => _ModelDataShowState();
+}
+
+class _ModelDataShowState extends State<ModelDataShow> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
